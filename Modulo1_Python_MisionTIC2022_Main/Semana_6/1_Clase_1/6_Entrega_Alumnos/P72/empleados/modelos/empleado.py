fcf511fb7d2f7d8257dc01feac8781fa19a1c5a6
luisguillermomolero <luisguillermomolero@gmail.com>
2022-05-23 07:48:56 -0500

    Third commit

--- Modulo1_Python_MisionTIC2022_Main/Semana_6/1_Clase_1/6_Entrega_Alumnos/P72/empleados/modelos/empleado.py
+++
@@ -1,6 +0,0 @@
-class Empleado:
-    def __init__(self, nombre, documento, correo, especialidad):
-        self.nombre = nombre
-        self.documento = documento
-        self.correo = correo
-        self.especialidad = especialidad
